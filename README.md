6bf5082695229499041f0baecdd5d091181b5dcd
jia_zhang@trendmicro.com.cn <jia_zhang@trendmicro.com.cn>
2020-08-07 10:04:09 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # jia-stk
+
+#### Objective
+
+  This repo is for dump stock info from different site and do some analysis
+
+#### Sites to get info from
+
+```
+Tushare: To get the stock list
+Sina: To get the daily info
+163: To get the history data
+```
+
+#### Programs
+
+```
+dump.py - To dump daily data
+```
+
+
+
+#### 
--- dump.py
+++ dump.py
@@ -1,10 +1,13 @@
+#This program need to be executed everyday to save data to db
 import sys
 sys.path.append('./lib')
 from lib.stock_info import StockInfo
 from lib.db import Db
 import pandas as pd
+import datetime
 
-def dump():
+#Dump daily data to database, table is by date...
+def dump_daily_data(table_name):
     db_ip = "10.64.165.81"
     db = Db()
     db_engine = db.connect("mysql+pymysql://root:mariadb@%s:3306/jia-stk"%db_ip)
@@ -12,7 +15,8 @@ def dump():
     for i in range(5):
         info = stock_info.get_daily_info_by_page(i)
         data = pd.read_json(info)
-        data.to_sql('stock_daily',db_engine, index=False, if_exists='append', chunksize=5000)
+        data.to_sql(table_name,db_engine, index=False, if_exists='append', chunksize=5000)
 
 if __name__ == '__main__':
-    dump()
+    today = datetime.datetime.now().strftime("%Y%m%d")
+    dump_daily_data(today)
